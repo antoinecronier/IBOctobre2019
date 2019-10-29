c7ca7650b362930165331ea495d00aa09c2a7fdc
harckline <antoinecronier1@gmail.com>
2019-10-29 15:09:21 +0100

    Module4TP1

---
+++ Module4TP1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module4TP1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            V1();
+            try
+            {
+                V2();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+
+        private static void V1()
+        {
+            int number = int.MaxValue;
+
+            Console.WriteLine(number);
+            Console.WriteLine(++number);
+            Console.ReadLine();
+        }
+
+        private static void V2()
+        {
+            checked
+            {
+                int number = int.MaxValue;
+
+                Console.WriteLine(number);
+                Console.WriteLine(++number);
+                // Console.WriteLine(checked(++number));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
